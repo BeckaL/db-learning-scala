13994a92a516762e0ec36b472f60f66ac461c5de
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2023-11-07 17:04:45 +0000

    Implement reading from file

--- src/main/scala/DatabaseFileUtils.scala
+++ src/main/scala/DatabaseFileUtils.scala
@@ -1,26 +1,57 @@
 import cats.effect.IO
 import model.{DatabaseMetadata, LogFile}
 
+import java.nio.ByteBuffer
 import java.nio.channels.FileChannel
+import java.nio.charset.{Charset, StandardCharsets}
 import java.nio.file.{Files, Path, Paths, StandardOpenOption}
 import scala.util.Try
 
-val FIRST_LOG_FILE = "logFile1.txt"
 def createDatabaseEngine(locationPrefix: String = "./src/main/resources", name: String): IO[DatabaseMetadata] =
+  val fileName = "logFile1.txt"
   val directoryPathString = locationPrefix + "/" + name
 
   for {
     directoryPath <- tryIO(Paths.get(directoryPathString))
-    filePath      <- tryIO(Paths.get(directoryPathString + "/" + FIRST_LOG_FILE))
+    filePath      <- tryIO(Paths.get(directoryPathString + "/" + fileName))
     path          <- tryIO(Files.createDirectory(directoryPath))
     filePath      <- tryIO(Files.createFile(filePath))
-  } yield DatabaseMetadata(directoryPath, List(LogFile(FIRST_LOG_FILE, Map())))
+  } yield DatabaseMetadata(directoryPath, List(LogFile(fileName, Map())))
 
 def writeToFile(stringToWrite: String, location: Path): IO[Long] =
   for {
     index <- tryIO(Files.size(location))
     file <- tryIO(Files.writeString(location, stringToWrite, StandardOpenOption.APPEND))
   } yield index
+
+def readFromFile(offset: Long, location: Path): IO[(String, String)] = {
+  for {
+    fileChannel: FileChannel <- tryIO(FileChannel.open(location))
+    _                        = fileChannel.position(offset)
+    keySize                  <- readBinaryIntegerFromFile(fileChannel)
+    key                      <- tryIO(readChunkFromFile(keySize, fileChannel))
+    valueSize                <- readBinaryIntegerFromFile(fileChannel)
+    value                    <- tryIO(readChunkFromFile(valueSize, fileChannel))
+    _                        = fileChannel.close()
+  } yield (key, value)
+}
+
+private def readBinaryIntegerFromFile(fileChannel: FileChannel): IO[Integer] =
+  for {
+    binaryString: String <- tryIO(readChunkFromFile(8, fileChannel))
+    integer <- tryIO(Integer.parseInt(binaryString, 2))
+  } yield integer
+
+
+private def readChunkFromFile(byteBufferSize: Int, fileChannel: FileChannel): String = {
+  val buffer = ByteBuffer.allocate(byteBufferSize)
+  while (buffer.hasRemaining) {
+    fileChannel.read(buffer)
+  }
+  buffer.position(0)
+  buffer.limit(byteBufferSize)
+  Charset.forName("UTF-8").decode(buffer).toString
+}
 
 private def tryIO[T](f: T): IO[T] = IO.fromTry(Try(f))
 
--- src/test/scala/DatabaseFileUtilsTest.scala
+++ src/test/scala/DatabaseFileUtilsTest.scala
@@ -3,18 +3,22 @@ import model.{DatabaseMetadata, LogFile}
 import org.scalatest.BeforeAndAfterEach
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
 
 import java.nio.file.{Files, Path, Paths}
 
-class DatabaseFileUtilsTest extends AnyFlatSpec with Matchers with BeforeAndAfterEach {
-  val prefix       = "./src/test/resources"
-  val logFile      = "logFile1.txt"
-  val databaseName = "myDatabase"
-  val existingDatabasePath = Paths.get(s"$prefix/existingDatabase")
-  val databasePath  = Paths.get(s"$prefix/$databaseName")
-  val logFilePath = Paths.get(s"$prefix/$databaseName/$logFile")
-  val existingLogFilePath = Paths.get(s"$prefix/existingDatabase/$logFile")
-
+class DatabaseFileUtilsTest extends AnyFlatSpec with Matchers with BeforeAndAfterEach with TableDrivenPropertyChecks {
+  private val prefix               = "./src/test/resources"
+  private val logFile              = "logFile1.txt"
+  private val databaseName         = "myDatabase"
+  private val existingDatabasePath = Paths.get(s"$prefix/existingDatabase")
+  private val databasePath         = Paths.get(s"$prefix/$databaseName")
+  private val logFilePath          = Paths.get(s"$prefix/$databaseName/$logFile")
+  private val existingLogFilePath  = Paths.get(s"$prefix/existingDatabase/$logFile")
+  private val myKey                = "myKey"
+  private val keySize              = "00000101"
+  private val myValue              = "myValue"
+  private val valueSize            = "00000111"
 
   "create database engine" should "create a new folder and log file" in {
     val expectedDatabase = DatabaseMetadata(databasePath, List(LogFile(logFile, Map())))
@@ -24,14 +28,14 @@ class DatabaseFileUtilsTest extends AnyFlatSpec with Matchers with BeforeAndAfte
   }
 
   "writeToFile" should "write to file and return the index it wrote to" in {
-    val db = DatabaseMetadata(existingDatabasePath, List(LogFile(logFile, Map())))
+    val db            = DatabaseMetadata(existingDatabasePath, List(LogFile(logFile, Map())))
     val stringToWrite = "someStringWithIndices"
     writeToFile(stringToWrite, existingLogFilePath).unsafeRunSync() shouldBe 0
     Files.readString(existingLogFilePath) shouldBe stringToWrite
   }
 
   it should "write to a file with existing content in and return the index it wrote to" in {
-    val db = DatabaseMetadata(existingDatabasePath, List(LogFile(logFile, Map())))
+    val db            = DatabaseMetadata(existingDatabasePath, List(LogFile(logFile, Map())))
     val stringToWrite = "someStringWithIndices"
 
     val i1 = writeToFile(stringToWrite, existingLogFilePath).unsafeRunSync()
@@ -40,6 +44,39 @@ class DatabaseFileUtilsTest extends AnyFlatSpec with Matchers with BeforeAndAfte
     Files.readString(existingLogFilePath) shouldBe stringToWrite + stringToWrite
     i1 shouldBe 0
     i2 shouldBe stringToWrite.size
+  }
+
+  "readFromFile" should "read bytes from a file" in {
+    val stringToWrite = keySize + myKey + valueSize + myValue
+
+    writeToFile(stringToWrite, existingLogFilePath).unsafeRunSync()
+
+    readFromFile(0, existingLogFilePath).unsafeRunSync() shouldBe (myKey, myValue)
+  }
+
+  it should "read bytes from a file when the offset is greater than 0" in {
+    val firstKeyValueString = "someKeyValueString"
+    val otherKeyValueString = keySize + myKey + valueSize + myValue
+
+    writeToFile(firstKeyValueString + otherKeyValueString, existingLogFilePath).unsafeRunSync()
+
+    readFromFile(firstKeyValueString.length, existingLogFilePath).unsafeRunSync() shouldBe (myKey, myValue)
+  }
+
+  it should "return an error if the keySize or valueSize is not an 8 padded length binary string" in {
+    // TODO make the result nicer e.g. either
+    val notACorrectlySizedBinaryString = "000"
+
+    val incorrectlyFormattedThings = List(
+      notACorrectlySizedBinaryString + myKey + valueSize + myValue,
+      keySize + myKey + notACorrectlySizedBinaryString + myValue
+    )
+
+    incorrectlyFormattedThings.foreach(s => {
+      writeToFile(s, existingLogFilePath).unsafeRunSync()
+      assertThrows[NumberFormatException](readFromFile(0, existingLogFilePath).unsafeRunSync())
+      Files.writeString(existingLogFilePath, "")
+    })
   }
 
   override def afterEach(): Unit = {
