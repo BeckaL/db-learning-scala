076cc47f5f3ee668cbb7799d600a465e308567d9
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2023-11-10 15:18:30 +0000

    Swap out TreeMap for SSTDatabaseMetadata

--- src/main/scala/SSTKeyValueStore/SSTDatabaseEngine.scala
+++ src/main/scala/SSTKeyValueStore/SSTDatabaseEngine.scala
@@ -4,11 +4,11 @@ import model.{DatabaseException, KeyNotFoundInIndices}
 
 import scala.collection.immutable.TreeMap
 
-def write(treeMap: TreeMap[String, String], key: String, value: String): TreeMap[String, String] =
-  treeMap.updated(key, value)
+def write(metadata: SSTDatabaseMetadata, key: String, value: String): TreeMap[String, String] =
+  metadata.memTable.updated(key, value)
 
-def read(treeMap: TreeMap[String, String], key: String): Either[DatabaseException, String] =
-  treeMap.get(key) match {
+def read(metadata: SSTDatabaseMetadata, key: String): Either[DatabaseException, String] =
+  metadata.memTable.get(key) match {
     case Some(value) => Right(value)
     case None        => Left(KeyNotFoundInIndices(key))
   }
--- src/test/scala/SSTKeyValueStore/SSTDatabaseEngineIntegrationTest.scala
+++ src/test/scala/SSTKeyValueStore/SSTDatabaseEngineIntegrationTest.scala
@@ -10,15 +10,15 @@ class SSTDatabaseEngineIntegrationTest extends AnyFlatSpec with Matchers {
   private val myKey   = "myKey"
   private val myValue = "myValue"
   "write" should "write to a thing" in {
-    write(TreeMap(), myKey, myValue) shouldBe TreeMap(myKey -> myValue)
+    write(SSTDatabaseMetadata(TreeMap(), List()), myKey, myValue) shouldBe TreeMap(myKey -> myValue)
   }
 
   "read" should "read from the map" in {
-    val tMap = TreeMap(myKey -> myValue)
-    read(tMap, myKey) shouldBe Right(myValue)
+    val memTable = TreeMap(myKey -> myValue)
+    read(SSTDatabaseMetadata(memTable, List()), myKey) shouldBe Right(myValue)
   }
 
   it should "return a left when the value is not found" in {
-    read(TreeMap(), myKey) shouldBe Left(KeyNotFoundInIndices(myKey))
+    read(SSTDatabaseMetadata(TreeMap(), List()), myKey) shouldBe Left(KeyNotFoundInIndices(myKey))
   }
 }
