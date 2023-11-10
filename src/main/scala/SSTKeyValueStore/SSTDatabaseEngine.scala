da9014e7fe1b4b9510652451e61de25b2710d0a8
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2023-11-10 15:36:18 +0000

    Implement reading from a log file when key is in compressed index

--- src/main/scala/SSTKeyValueStore/SSTDatabaseEngine.scala
+++ src/main/scala/SSTKeyValueStore/SSTDatabaseEngine.scala
@@ -1,14 +1,25 @@
 package SSTKeyValueStore
 
-import model.{DatabaseException, KeyNotFoundInIndices}
+import cats.effect.IO
+import model.{DatabaseException, KeyNotFoundInIndices, LogFile}
+import shared.readFromFile
 
 import scala.collection.immutable.TreeMap
 
 def write(metadata: SSTDatabaseMetadata, key: String, value: String): TreeMap[String, String] =
   metadata.memTable.updated(key, value)
 
-def read(metadata: SSTDatabaseMetadata, key: String): Either[DatabaseException, String] =
+def read(metadata: SSTDatabaseMetadata, key: String): IO[Either[DatabaseException, String]] =
   metadata.memTable.get(key) match {
-    case Some(value) => Right(value)
-    case None        => Left(KeyNotFoundInIndices(key))
+    case Some(value) => IO.pure(Right(value))
+    case None        => attemptToReadFromLogFiles(metadata.logFiles, key)
+  }
+
+private def attemptToReadFromLogFiles(logFiles: List[LogFile], key: String): IO[Either[DatabaseException, String]] =
+  logFiles match {
+    case firstLogFile :: others => firstLogFile.index.get(key) match {
+      case None => attemptToReadFromLogFiles(others, key)
+      case Some(offset) => readFromFile(offset, firstLogFile.path).map(_.map(_._2))
+    }
+    case Nil => IO.pure(Left(KeyNotFoundInIndices(key)))
   }
--- src/test/scala/SSTKeyValueStore/SSTDatabaseEngineIntegrationTest.scala
+++ src/test/scala/SSTKeyValueStore/SSTDatabaseEngineIntegrationTest.scala
@@ -1,24 +1,41 @@
 package SSTKeyValueStore
 
-import model.KeyNotFoundInIndices
+import cats.effect.unsafe.implicits.global
+import model.{KeyNotFoundInIndices, LogFile}
+import org.scalatest.BeforeAndAfterEach
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import shared.getStringToWrite
 
+import java.nio.file.{Files, Paths}
 import scala.collection.immutable.TreeMap
 
-class SSTDatabaseEngineIntegrationTest extends AnyFlatSpec with Matchers {
+class SSTDatabaseEngineIntegrationTest extends AnyFlatSpec with Matchers with BeforeAndAfterEach{
   private val myKey   = "myKey"
   private val myValue = "myValue"
+  private val databasePath = Paths.get("./src/test/resources/SSTDatabaseEngineIntegrationTest")
+  private val existingLogFile = Paths.get(databasePath.toString + "/" + "logFile1.txt")
   "write" should "write to a thing" in {
     write(SSTDatabaseMetadata(TreeMap(), List()), myKey, myValue) shouldBe TreeMap(myKey -> myValue)
   }
 
   "read" should "read from the map" in {
     val memTable = TreeMap(myKey -> myValue)
-    read(SSTDatabaseMetadata(memTable, List()), myKey) shouldBe Right(myValue)
+    read(SSTDatabaseMetadata(memTable, List()), myKey).unsafeRunSync() shouldBe Right(myValue)
   }
 
-  it should "return a left when the value is not found" in {
-    read(SSTDatabaseMetadata(TreeMap(), List()), myKey) shouldBe Left(KeyNotFoundInIndices(myKey))
+  it should "read from a logfile when the entry is directly in the index" in {
+    Files.writeString(existingLogFile, getStringToWrite(myKey, myValue).getOrElse(throw new RuntimeException("oops")))
+    val logFile = LogFile(existingLogFile, Map(myKey -> 0))
+    val metadata = SSTDatabaseMetadata(TreeMap("anotherKey" -> "anotherValue"), List(logFile))
+
+    read(metadata, myKey).unsafeRunSync() shouldBe Right(myValue)
   }
+
+  it should "return a left when the value is not found in the memtable and there are no logs" in {
+    read(SSTDatabaseMetadata(TreeMap(), List()), myKey).unsafeRunSync() shouldBe Left(KeyNotFoundInIndices(myKey))
+  }
+
+  override def afterEach(): Unit =
+    Files.writeString(existingLogFile, "")
 }
