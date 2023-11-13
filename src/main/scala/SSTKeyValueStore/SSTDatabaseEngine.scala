5d20eb5ccd388b815931ea63d3798fe4473c4f78
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2023-11-13 12:07:29 +0000

    SSTDatabaseEngine reads from file when a key is not directly in the index

--- src/main/scala/SSTKeyValueStore/SSTDatabaseEngine.scala
+++ src/main/scala/SSTKeyValueStore/SSTDatabaseEngine.scala
@@ -2,7 +2,7 @@ package SSTKeyValueStore
 
 import cats.effect.IO
 import model.{DatabaseException, KeyNotFoundInIndices, LogFile}
-import shared.readFromFile
+import shared.{readFromFile, scanFileForKey}
 
 import scala.collection.immutable.TreeMap
 
@@ -16,10 +16,26 @@ def read(metadata: SSTDatabaseMetadata, key: String): IO[Either[DatabaseExceptio
   }
 
 private def attemptToReadFromLogFiles(logFiles: List[LogFile], key: String): IO[Either[DatabaseException, String]] =
-  logFiles match {
-    case firstLogFile :: others => firstLogFile.index.get(key) match {
-        case None         => attemptToReadFromLogFiles(others, key)
+  logFiles match
+    case firstLogFile :: others => firstLogFile.index.get(key) match
         case Some(offset) => readFromFile(offset, firstLogFile.path).map(_.map(_._2))
-      }
+        case None =>
+          if (firstLogFile.index.nonEmpty)
+            findOffsetToScan(firstLogFile.index, key) match
+              case (startOffset, Some(endOffset)) =>
+                scanFileForKey(startOffset, endOffset, firstLogFile.path, key).flatMap {
+                  case Right(value)                  => IO.pure(Right(value))
+                  case Left(KeyNotFoundInIndices(_)) => attemptToReadFromLogFiles(others, key)
+                  case Left(otherException)          => ???
+                }
+              case _ => ???
+          else attemptToReadFromLogFiles(others, key)
     case Nil => IO.pure(Left(KeyNotFoundInIndices(key)))
-  }
+
+private def findOffsetToScan(index: Map[String, Long], keyToSearchFor: String): (Long, Option[Long]) =
+  val sortedKeys = index.keys.toList.sorted
+  val maybeNearestKeyAndNext =
+    sortedKeys.dropRight(1).zip(sortedKeys.tail).find((key, next) => keyToSearchFor > key && keyToSearchFor < next)
+  maybeNearestKeyAndNext match
+    case Some((key, nextKey)) => (index(key), Some(index(nextKey)))
+    case None                 => (index(sortedKeys.last), None)
--- src/test/scala/SSTKeyValueStore/SSTDatabaseEngineIntegrationTest.scala
+++ src/test/scala/SSTKeyValueStore/SSTDatabaseEngineIntegrationTest.scala
@@ -15,7 +15,7 @@ class SSTDatabaseEngineIntegrationTest extends AnyFlatSpec with Matchers with Be
   private val myValue         = "myValue"
   private val databasePath    = Paths.get("./src/test/resources/SSTDatabaseEngineIntegrationTest")
   private val existingLogFile = Paths.get(databasePath.toString + "/" + "logFile1.txt")
-  private val secondLogFile = Paths.get(databasePath.toString + "/" + "logFile2.txt")
+  private val secondLogFile   = Paths.get(databasePath.toString + "/" + "logFile2.txt")
   "write" should "write to a thing" in {
     write(SSTDatabaseMetadata(TreeMap(), List()), myKey, myValue) shouldBe TreeMap(myKey -> myValue)
   }
@@ -35,16 +35,49 @@ class SSTDatabaseEngineIntegrationTest extends AnyFlatSpec with Matchers with Be
 
   it should "read from a logfile when the entry is directly in an older index" in {
     Files.writeString(existingLogFile, getStringToWrite(myKey, myValue).getOrElse(throw new RuntimeException("oops")))
-    val logFile = LogFile(existingLogFile, Map(myKey -> 0))
+    val logFile  = LogFile(existingLogFile, Map(myKey -> 0))
     val metadata = SSTDatabaseMetadata(TreeMap("anotherKey" -> "anotherValue"), List(LogFile.empty(secondLogFile), logFile))
 
     read(metadata, myKey).unsafeRunSync() shouldBe Right(myValue)
+  }
+
+  it should "read from a logfile when the entry is not directly in the index" in {
+    val keySize   = 5
+    val valueSize = 10
+    val toWrite = ('a' to 'z').toList
+      .map(char => getStringToWrite(char.toString * keySize, char.toString * valueSize).getRight)
+    Files.writeString(existingLogFile, toWrite.mkString(""))
+
+    def getOffsetOf(string: String) = {
+      val keyValueStringLength = keySize + valueSize + 16
+      ('a' to 'z').toList.indexOf(string.head) * keyValueStringLength
+    }
+
+    val index: Map[String, Long] = Map("a" * 5 -> 0, "h" * 5 -> getOffsetOf("h"), "t" -> getOffsetOf("t"))
+    println(index)
+    println(getOffsetOf("b"))
+
+    val logFile             = LogFile(existingLogFile, index)
+    val inMemoryIndex       = TreeMap("someKey" -> "someValue", "anotherKey" -> "anotherValue")
+    val sstDatabaseMetadata = SSTDatabaseMetadata(inMemoryIndex, List(logFile))
+
+    val keyToSearchFor = "r" * 5
+    val expectedValue  = "r" * 10
+
+    read(sstDatabaseMetadata, keyToSearchFor).unsafeRunSync() shouldBe Right(expectedValue)
   }
 
   it should "return a left when the value is not found in the memtable and there are no logs" in {
     read(SSTDatabaseMetadata(TreeMap(), List()), myKey).unsafeRunSync() shouldBe Left(KeyNotFoundInIndices(myKey))
   }
 
-  override def afterEach(): Unit =
+  override def afterEach(): Unit = {
     Files.writeString(existingLogFile, "")
+  }
+
+  implicit class EitherOps[A, B](e: Either[A, B]) {
+    def getRight = e.getOrElse(throw new RuntimeException("expected right but got left"))
+
+    def getLeft = e.left.getOrElse(throw new RuntimeException("expected left but got right"))
+  }
 }
